2279ecb9c8f565b84acd008667f0fc839da38a70
Muhammad Sohaib <muhammadsohaib030@gmail.com>
2022-09-22 18:14:20 +0500

    service file added

---
+++ lib/Services/state_services.dart
@@ -0,0 +1,19 @@
+import 'dart:convert';
+
+import 'package:covid_19_app/Model/world_states_model.dart';
+import 'package:covid_19_app/Services/Utilities/app_url.dart';
+
+import 'package:http/http.dart' as http;
+
+class WorldStateService {
+  // ignore: non_constant_identifier_names
+  Future<WorldState> FetchWorldStateRecord() async {
+    final response = await http.get(Uri.parse(AppUrl.worldStatesApi.toString()));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      return WorldState.fromJson(data);
+    } else {
+      throw Exception("error");
+    }
+  }
+}
