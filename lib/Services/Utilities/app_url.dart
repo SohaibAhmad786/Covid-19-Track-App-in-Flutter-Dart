a5be52d7550f8ae1c9e4d9ef7a1a1cc2831aa076
Muhammad Sohaib <muhammadsohaib030@gmail.com>
2022-09-22 18:19:03 +0500

    Services Modified

--- lib/Services/Utilities/app_url.dart
+++ lib/Services/Utilities/app_url.dart
@@ -2,7 +2,6 @@ class AppUrl {
   static const String baseUrl = 'https://disease.sh/v3/covid-19/';
 
   // fetch world covid states
-  static const String getLink = 'https://disease.sh/v3/covid-19/all';
   static const String worldStatesApi = '${baseUrl}all';
   static const String countriesList = '${baseUrl}countries';
 }
